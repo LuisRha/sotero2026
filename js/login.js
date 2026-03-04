94abbfb063796adeb2dffe29d74afe0092665f0d
LuisRha <127280301+LuisRha@users.noreply.github.com>
2026-03-03 22:35:50 -0500

    ikjhlkjh

---
+++ js/login.js
@@ -0,0 +1,30 @@
+const SUPABASE_URL = "https://caovuekqrczqysxgnucc.supabase.co";
+const SUPABASE_KEY = "sb_publishable_843ipMaoEhnMrvuF95Iq6Q_9It7qiFX";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
+
+document.getElementById("btnLogin").addEventListener("click", login);
+
+async function login(){
+
+const email = document.getElementById("email").value;
+const password = document.getElementById("password").value;
+
+const { data, error } = await supabase.auth.signInWithPassword({
+email: email,
+password: password
+});
+
+if(error){
+
+document.getElementById("msg").innerText = "Credenciales incorrectas";
+return;
+
+}
+
+window.location.replace("/admin.html");
+
+}
