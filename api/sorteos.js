ce1a5138eef624c251ae44cf5e3a5355e109e6c8
LuisRha <127280301+LuisRha@users.noreply.github.com>
2026-03-06 16:06:58 -0500

    kjhlkjñlj

--- api/sorteos.js
+++ api/sorteos.js
@@ -1,10 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-  "https://xxxx.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE
 );
-
 
 export default async function handler(req, res) {
 
