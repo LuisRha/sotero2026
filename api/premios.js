b76499886b83050789c1fb8a648d2bde8d52f658
LuisRha <127280301+LuisRha@users.noreply.github.com>
2026-03-07 12:49:53 -0500

    jhijknbkjnb

---
+++ api/premios.js
@@ -0,0 +1,38 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE
+);
+
+export default async function handler(req, res) {
+
+  try {
+
+    if (req.method === "GET") {
+
+      const { data, error } = await supabase
+        .from("tickets")
+        .select("numero,premio")
+        .order("numero",{ascending:true});
+
+      if(error){
+        return res.status(500).json({error:error.message});
+      }
+
+      return res.status(200).json(data);
+
+    }
+
+    return res.status(405).json({error:"Método no permitido"});
+
+  } catch(err){
+
+    return res.status(500).json({
+      error:"Error interno",
+      detalle:err.message
+    });
+
+  }
+
+}
