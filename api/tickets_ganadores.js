1ae2464e28f6d5df1bcc6abf3b781b17f0187a7e
LuisRha <127280301+LuisRha@users.noreply.github.com>
2026-03-10 00:51:05 -0500

    hjkhgh

--- api/tickets_ganadores.js
+++ api/tickets_ganadores.js
@@ -7,10 +7,27 @@ const supabase = createClient(
 
 export default async function handler(req, res) {
 
-  const { data, error } = await supabase
-    .from("tickets")
-    .select("*");
+  try {
 
-  res.status(200).json(data);
+    const { data, error } = await supabase
+      .from("tickets")
+      .select("numero, ganador, telefono")
+      .eq("premio", true)
+      .eq("usado", true);
+
+    if (error) {
+      return res.status(500).json({ error: error.message });
+    }
+
+    res.status(200).json(data);
+
+  } catch (err) {
+
+    res.status(500).json({
+      error: "Error interno",
+      detalle: err.message
+    });
+
+  }
 
 }
