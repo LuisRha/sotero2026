eec51906c25d4ab5def70ca0ca3542b9d030ddd7
LuisRha <127280301+LuisRha@users.noreply.github.com>
2026-07-28 09:55:40 -0500

    errore ticket y enviar correo

--- api/tickets_ganadores.js
+++ api/tickets_ganadores.js
@@ -1,39 +1,39 @@
-import { createClient } from "@supabase/supabase-js";
+// import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE
-);
+// const supabase = createClient(
+//   process.env.SUPABASE_URL,
+//   process.env.SUPABASE_SERVICE_ROLE
+// );
 
-export default async function handler(req, res) {
+// export default async function handler(req, res) {
 
-  try {
+//   try {
 
-    // Solo permitir GET
-    if(req.method !== "GET"){
-      return res.status(405).json({ error: "Método no permitido" });
-    }
+//     // Solo permitir GET
+//     if(req.method !== "GET"){
+//       return res.status(405).json({ error: "Método no permitido" });
+//     }
 
-    const { data, error } = await supabase
-      .from("tickets")
-      .select("numero, ganador, telefono, compra_id, created_at")
-      .eq("premio", true)
-      .eq("usado", true)
-      .order("created_at", { ascending: false });
+//     const { data, error } = await supabase
+//       .from("tickets")
+//       .select("numero, ganador, telefono, compra_id, created_at")
+//       .eq("premio", true)
+//       .eq("usado", true)
+//       .order("created_at", { ascending: false });
 
-    if (error) {
-      return res.status(500).json({ error: error.message });
-    }
+//     if (error) {
+//       return res.status(500).json({ error: error.message });
+//     }
 
-    res.status(200).json(data);
+//     res.status(200).json(data);
 
-  } catch (err) {
+//   } catch (err) {
 
-    res.status(500).json({
-      error: "Error interno",
-      detalle: err.message
-    });
+//     res.status(500).json({
+//       error: "Error interno",
+//       detalle: err.message
+//     });
 
-  }
+//   }
 
-}
+// }
